de8a9ce457ec9ca38a152a4ac75d88f9777567dc
Troy H. Deck <troy.deque@gmail.com>
2024-08-15 14:53:52 -0700

    More precision using us x 4; needs cleanup + backlash comp

--- firmware/control/control.ino
+++ firmware/control/control.ino
@@ -26,20 +26,30 @@ const char* CELL_CHARS = ",#L ;8-#R4TB\"1.OJFEICDGHWPA.XYQ$N$M%ZS?V+K.U`";
 
 // Servo info
 const uint16_t US_90DEG = 1500; // Should not need to be changed
-const int US_OFFSET = 0;
-const int US_PER_COL = 44; // Orig 45
-// Testing
-// 45: R great, U is a column off
-// 44: U is great,
+const int US_OFFSET = 0; // TODO was -10
+// We pre-multiply this by 4 so we can get better precision when accumulating; then
+// we bit shift after multiplying
+const int16_t US_PER_COL_TIMES_4 = 175; 
+// Testing (offset 0)
+// 42 pretty good, cap sign a bit too far right, G a bit too far right, U looks good
+// 41 R a bit too far left, G a bit right, K way too far right
+// Offset 20
+// 42 pretty good, U a bit too far left, R a bit too far left
+// Offset 10
+// 42 is pretty good!
 
-const int US_BACKLASH = 0; // I think the right value is around 20
+
+const int US_BACKLASH = 0; // TODO I think the right value is around 20
 
 // Other stuff
 const unsigned long DELAY_MS = 3000;
 
 // Computed values
-const int MID_POS = DRUM_COLS / 2;
-const uint16_t START_US = US_90DEG + MID_POS * US_PER_COL + US_OFFSET;
+const uint16_t MID_POS = DRUM_COLS / 2;
+const uint16_t START_US =
+    US_90DEG
+    + ((MID_POS * US_PER_COL_TIMES_4) >> 2)
+    + US_OFFSET;
 
 uint16_t last_pos_us = 0;
 int last_dir = 1; // Either -1 or +1
@@ -47,11 +57,11 @@ int last_dir = 1; // Either -1 or +1
 void setCell(char c) {
   // Find char pos w/o using whole string lib
   int p;
-  uint16_t us = START_US;
+  int16_t us_from_start_times_4 = 0;
   for (p = 0; p < DRUM_COLS - 1; ++ p) {
     if (CELL_CHARS[p] == c) {
       uint16_t prior_pos_us = last_pos_us;
-      last_pos_us = us;
+      //last_pos_us = us;
       
       //int dir = us > prior_pos_us ? 1 : -1;
       /*
@@ -70,14 +80,23 @@ void setCell(char c) {
         digitalWrite(LED_PIN, LOW);
       }*/
 
-      myservo.writeMicroseconds(MAX_PULSE_WIDTH);
-      delay(100);
+      uint16_t us = START_US + (us_from_start_times_4 >> 2); // Note: This >> is an arithmetic shift
+      /* Backlash compensated
+      if (p >= MID_POS) {
+        myservo.writeMicroseconds(MAX_PULSE_WIDTH);
+      } else {
+        myservo.writeMicroseconds(MIN_PULSE_WIDTH);
+        us = us + US_BACKLASH;
+      }
+      delay(400);
+      */
+
       
       myservo.writeMicroseconds(us);  
       //last_dir = dir;
       return;
     }
-    us -= US_PER_COL;
+    us_from_start_times_4 -= US_PER_COL_TIMES_4;
   }
 }
 
@@ -93,20 +112,17 @@ void showMessage(char* text, uint16_t delay_ms) {
 
 void loop() {
   // NOTE led not working
-  /*
-  digitalWrite(LED_PIN, LOW);
-  myservo.writeMicroseconds(US_90DEG);
-  delay( 2* DELAY_MS);
 
-  digitalWrite(LED_PIN, HIGH);
-  myservo.writeMicroseconds(US_90DEG + US_BACKLASH);
-  delay(DELAY_MS);
-  */
+  //digitalWrite(LED_PIN, LOW);
+  myservo.writeMicroseconds(US_90DEG);
+  delay(1000);
 
   //showMessage("BOOGER ", DELAY_MS);
 
-  setCell('R');
-  delay(DELAY_MS);
+  //showMessage("#R4TB ", DELAY_MS);
+
+  //showMessage("GTB\"", DELAY_MS);
+  showMessage("WORKING.", DELAY_MS);
 
   /*
   setCell('A');
